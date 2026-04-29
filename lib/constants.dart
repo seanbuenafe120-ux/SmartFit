79ef0a227c5f7f5e09bf615fd82134a43f14a368
JohnCedricTaopo_Dev <80130772+JohnCedricTaopo@users.noreply.github.com>
2026-04-30 02:46:12 +0800

    Add GymAppColors class with color constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GymAppColors {
+
+  static const Color primaryBlue = Color(0xFF007BFF);      
+  static const Color darkBg = Color(0xFF0A0A0A);          
+  static const Color cardBg = Color(0xFF1A1A1A);          
+  static const Color accentGreen = Color(0xFF10B981);      
+  static const Color accentOrange = Color(0xFFFF6B35);     
+  static const Color accentCyan = Color(0xFF00D4FF);       
+  static const Color accentRed = Color(0xFFEF4444);        
+  static const Color textPrimary = Color(0xFFFFFFFF);     
+  static const Color textSecondary = Color(0xFF9CA3AF);   
+}
